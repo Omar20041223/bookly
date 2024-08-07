d329e1a431d1d9698bacbf2984d8203322f42c5a
DELL <omarmohamed01284@gmail.com>
2024-08-07 16:11:04 +0300

    some touches

--- lib/features/home/presentaions/manager/cubit/allbooks_cubit.dart
+++ lib/features/home/presentaions/manager/cubit/allbooks_cubit.dart
@@ -1,12 +1,13 @@
 import 'package:booklyinsession/core/errors/faliure.dart';
 import 'package:booklyinsession/features/home/data/home_repo/home_repo.dart';
+import 'package:booklyinsession/features/home/data/home_repo/home_repo_impl.dart';
 import 'package:booklyinsession/features/home/data/models/books/books/item.dart';
 import 'package:booklyinsession/features/home/presentaions/manager/cubit/allbooks_state.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AllbooksCubit extends Cubit<AllbooksState> {
-  final HomeRepo homeRepo;
+  final HomeRepoImpl homeRepo;
 
   AllbooksCubit(this.homeRepo) : super(AllbooksInitial());
 
@@ -15,8 +16,12 @@ class AllbooksCubit extends Cubit<AllbooksState> {
     final Either<Faliures, List<Item>> data = await homeRepo.fetchAllBooks();
 
     data.fold(
-          (failure) => emit(AllbooksFailure(failure.toString())),
-          (books) => emit(AllbooksSuccess(books)),
+          (failure) {
+            emit(AllbooksFailure(failure.toString()));
+          },
+          (books) {
+            emit(AllbooksSuccess(books));
+          },
     );
   }
 
@@ -25,8 +30,12 @@ class AllbooksCubit extends Cubit<AllbooksState> {
     final Either<Faliures, List<Item>> data = await homeRepo.fetchNewestBooks();
 
     data.fold(
-          (failure) => emit(AllbooksFailure(failure.toString())),
-          (books) => emit(AllbooksSuccess(books)),
+          (failure) {
+            emit(AllbooksFailure(failure.toString()));
+          },
+          (books) {
+            emit(AllbooksSuccess(books));
+          },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  final ApiService apiService = ApiService(dio: Dio());
-  final HomeRepoImpl homeRepoImpl = HomeRepoImpl(apiService: apiService);
-  runApp(MyApp(homeRepoImpl: homeRepoImpl));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final HomeRepoImpl homeRepoImpl;
+  const MyApp({super.key});
 
-  const MyApp({Key? key, required this.homeRepoImpl}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => AllbooksCubit(homeRepoImpl),
+      create: (context) => AllbooksCubit(HomeRepoImpl(apiService: ApiService(dio: Dio()))),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         routes: {
